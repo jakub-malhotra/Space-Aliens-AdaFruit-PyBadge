9362562901acace7e8349cdb540d3865cdabb430
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-04-23 11:06:11 -0400

    HELLO WORLD

--- code.py
+++ code.py
@@ -1,1 +1,27 @@
-print("Hello,World")
+#!/usr/bin/env python3
+
+"""
+Created: By Jakub Malhotra
+Created: On April 2024
+This is the "Space Aliens" game on the Adafruit Pybadge
+"""
+
+import stage
+import ugame
+
+
+def game_scene() -> None:
+    """
+    This is the main function game scene
+    """
+
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass  # Just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
