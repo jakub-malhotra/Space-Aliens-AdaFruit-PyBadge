5b8be8281e3211967c0fb4db63d086be7ecd08b1
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-05-01 20:26:02 -0400

    commit learning guide #2

--- code.py
+++ code.py
@@ -15,8 +15,23 @@ def game_scene() -> None:
     This is the main function game scene
     """
 
-    print("\n\n\n")  # 3 blank lines
-    print("Hello, World!")
+    # image bank for circuit python
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    # set the background to image 0 in the image bank
+    #   and set the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8 )
+
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+
+    # set the layers of all sprites, items show up in order 
+    game.layers = [background]
+
+    # render all sprites
+    #   most likley you will only render the background once per game scene 
+    game.render_block()
 
     # repeat forever, game loop
     while True:
