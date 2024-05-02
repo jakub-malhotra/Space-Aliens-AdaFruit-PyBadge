7b1dc80c591612244a81c60ae19c42f64f8b2408
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-05-02 17:36:18 -0400

    learning guide #4 complete

--- code.py
+++ code.py
@@ -15,19 +15,23 @@ def game_scene() -> None:
     This is the main function game scene
     """
 
-    # image bank for circuit python
+    # image banks for circuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # set the background to image 0 in the image bank
     #   and set the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8 )
+
+    # a sprite that will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
 
     # set the layers of all sprites, items show up in order 
-    game.layers = [background]
+    game.layers = [ship] + [background]
 
     # render all sprites
     #   most likley you will only render the background once per game scene 
@@ -35,7 +39,31 @@ def game_scene() -> None:
 
     # repeat forever, game loop
     while True:
-        pass  # Just a placeholder for now
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+
+        # update game logic
+
+        # redraw sprite
+        game.render_sprites([ship])
+        game.tick()  #wait until refresh rate finishes
 
 
 if __name__ == "__main__":
