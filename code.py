b4a7b3a955baa7105acf5d8cadaea107ed6a6348
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-05-07 21:21:38 -0400

    lesson #5

--- code.py
+++ code.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python3
 
 """
-Created: By Jakub Malhotra
-Created: On April 2024
-This is the "Space Aliens" game on the Adafruit Pybadge
+Created by: Jakub Malhotra
+Created on: May 2024
+This program is the "Space Aliens" program on the PyBadge
 """
 
+import constants
 import stage
 import ugame
 
-
 def game_scene() -> None:
     """
-    This is the main function game scene
+    This function is the main game game_scene
     """
 
-    # image banks for circuitPython
+    # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # set the background to image 0 in the image bank
-    #   and set the size (10x8 tiles of size 16x16)
-    background = stage.Grid(image_bank_background, 10, 8 )
+    # and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
 
     # a sprite that will be updated every frame
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
 
     # create a stage for the background to show up on
-    #   and set the frame rate to 60fps
+    # and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
 
-    # set the layers of all sprites, items show up in order 
+    # set the layers of all sprites, items show up in order
     game.layers = [ship] + [background]
 
     # render all sprites
-    #   most likley you will only render the background once per game scene 
+    # most likely you will only render the background once per game scene
     game.render_block()
 
     # repeat forever, game loop
@@ -42,29 +44,23 @@ def game_scene() -> None:
         # get user input
         keys = ugame.buttons.get_pressed()
 
-        if keys & ugame.K_X:
-            print("A")
-        if keys & ugame.K_O:
-            print("B")
-        if keys & ugame.K_START:
-            print("Start")
-        if keys & ugame.K_SELECT:
-            print("Select")
-        if keys & ugame.K_RIGHT:
-            ship.move(ship.x + 1, ship.y)
-        if keys & ugame.K_LEFT:
-            ship.move(ship.x - 1, ship.y)
-        if keys & ugame.K_UP:
-            ship.move(ship.x, ship.y - 1)
-        if keys & ugame.K_DOWN:
-            ship.move(ship.x, ship.y + 1)
+        if keys & ugame.K_RIGHT != 0:
+            if ship.x < (constants.SCREEN_X - constants.SPRITE_SIZE):
+                ship.move(ship.x + constants.SPRITE_MOVEMENT_SPEED, ship.y)
+            else:
+                ship.move((constants.SCREEN_X - constants.SPRITE_SIZE), ship.y)
+
+        if keys & ugame.K_LEFT != 0:
+            if ship.x > 0:
+                ship.move(ship.x - constants.SPRITE_MOVEMENT_SPEED, ship.y)
+            else:
+                ship.move(0, ship.y)
 
         # update game logic
 
-        # redraw sprite
+        # redraw Sprite
         game.render_sprites([ship])
-        game.tick()  #wait until refresh rate finishes
-
+        game.tick()  # wait until refresh rate finishes
 
 if __name__ == "__main__":
     game_scene()
---
+++ constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Created by: Jakub Malhotra
+Created on: May 2024
+This constants file is for Space Alien game
+"""
+
+# PyBadge screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
